fb0820091b41424e20d967067772f75b80d733f9
Edwer Soret <edwer2soret@gmail.com>
2024-05-02 21:22:55 -0400

    New

--- main.c
+++ main.c
@@ -7,10 +7,12 @@ int main()
 {	
 	Logger* logger = create_log(); //Crea el logger que utilizaremos
 	
-	Message prueba;
-	char string[]
+	Message prueba = {8, "Esto es "};
 	
-	add_character(prueba.string, prueba.size, )
+	
+	add_character(prueba.string, &prueba.size, 'C');
+	
+	
 	
 
 	liberate_memory(logger); //Libera la memoria que ocupaba el logger

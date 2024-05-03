4cf1f3844f065d47c7e3079b6dcdbac88f65fee7
Edwer Soret <edwer2soret@gmail.com>
2024-05-02 22:02:52 -0400

    new

--- main.c
+++ main.c
@@ -8,20 +8,10 @@ int main()
 	Logger* logger = create_log(); //Crea el logger que utilizaremos
 	
 	
-	Message* prueba = (Message*)calloc(1, sizeof(Message)); //Por que Jaffar crea todo como tipo apuntador?? No entiendo pero bueno //Y luego usa ->
-	
-	//Message prueba = {8, "Esto es "};
-	
-	//~ prueba->size = 8;
-	//~ prueba->string = "Esto es";
-	//~ print_string(prueba->string);
-	
-	
-	//~ add_character(prueba->string, &prueba->size, 'C');
-	//~ print_string(prueba->string);
-	
-	char string[10];
+	Message mensaje1 = {8};
+	mensaje1.string = (char*)calloc(mensaje1.size, sizeof(char));
 	I
+	
 
 	liberate_memory(logger); //Libera la memoria que ocupaba el logger
 	return 0;
